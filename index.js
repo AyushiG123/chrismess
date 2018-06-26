e03c39531aadaae2771134c24964b07ac1703759
Ayushi Gupta <ayushi.pixie@gmail.com>
2018-06-26 13:53:46 -0400

    Updating to classroom work (Day 2)

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
-const button1 = document.querySelector('button')
-const button2 = document.querySelector('button.dragon')
+
 const form = document.querySelector('form#flickForm')
 
-const changeHeading1 = function(){
-  const h1 = document.querySelector('h1')
-  h1.textContent = 'Merry'
-}
-
-const changeHeading2 = function(){
-  const h2 = document.querySelector('h1.fire')
-  h2.textContent = 'Christmas'
-}
-
-const changeHeading3 = function(event){
+const changeHeading = function(event){
   event.preventDefault()
   const f = event.target
-  const h3 = document.querySelector('h1')
-  h3.textContent = f.value
+
+  const flickName = f.flickName.value
+  const item = document.createElement('li')
+  item.textContent = flickName
+
+  const list = document.querySelector('#flicks')
+  list.appendChild(item)
+
+  f.reset()
 }
-button1.addEventListener('click', changeHeading1)
-button2.addEventListener('click', changeHeading2)
-form.addEventListener('submit', changeHeading3)
+
+form.addEventListener('submit', changeHeading)
