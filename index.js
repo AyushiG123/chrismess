144a21d5761f555da559f15b4c18c5355304791e
Ayushi Gupta <ayushi.pixie@gmail.com>
2018-06-26 14:46:33 -0400

    Display second field in separate HTML element

--- index.js
+++ index.js
@@ -5,23 +5,12 @@ const  addItems= function(event){
   event.preventDefault()
   const f = event.target
 
-  const flickName = f.flickName1.value
-  const aFlickName = f.flickName2.value
-  const item1 = document.createElement('li')
-  const item2 = document.createElement('li')
-  item1.textContent = flickName
-  item2.textContent = aFlickName
+  const flicksDiv = document.querySelector('#flicks1')
+  flicksDiv.textContent += ' '+f.flickName1.value
 
-  const list = document.querySelector('#flicks')
-  if(aFlickName=='')
-  list.appendChild(item1)
-  else if(flickName=='')
-  list.appendChild(item2)
-  else
-  {
-    list.appendChild(item1)
-    list.appendChild(item2)
-  }
+  const flicksDiv2 = document.querySelector('#flicks2')
+  flicksDiv2.textContent += ' '+f.flickName2.value
+
   f.reset()
 }
 
