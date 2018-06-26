f57f725705b4d4f30abf50e6236d98b3a655ce89
Ayushi Gupta <ayushi.pixie@gmail.com>
2018-06-26 14:22:20 -0400

    Add a second field to the form and display its value in the list (HW 2)

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 
 const form = document.querySelector('form#flickForm')
 
-const changeHeading = function(event){
+const  addItems= function(event){
   event.preventDefault()
   const f = event.target
 
-  const flickName = f.flickName.value
-  const item = document.createElement('li')
-  item.textContent = flickName
+  const flickName = f.flickName1.value
+  const aFlickName = f.flickName2.value
+  const item1 = document.createElement('li')
+  const item2 = document.createElement('li')
+  item1.textContent = flickName
+  item2.textContent = aFlickName
 
   const list = document.querySelector('#flicks')
-  list.appendChild(item)
-
+  if(aFlickName=='')
+  list.appendChild(item1)
+  else if(flickName=='')
+  list.appendChild(item2)
+  else
+  {
+    list.appendChild(item1)
+    list.appendChild(item2)
+  }
   f.reset()
 }
 
-form.addEventListener('submit', changeHeading)
+form.addEventListener('submit', addItems)
