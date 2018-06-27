a44077e214af7db69318025aed031ab9f3180eb8
Ayushi Gupta <ayushi.pixie@gmail.com>
2018-06-27 14:56:12 -0400

    Store each flick in an array

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
+class App{
+  constructor(){
+    const form = document.querySelector('form#flickForm')
+    form.addEventListener('submit', (ev) =>{
+      ev.preventDefault()
+      this.addItems(ev)
+    })
+    this.arr=[]
+  }
 
-const form = document.querySelector('form#flickForm')
 
-const  addItems= function(event){
-  event.preventDefault()
-  const f = event.target
-
-  const flicksDiv = document.querySelector('#flicks1')
-  flicksDiv.textContent += ' '+f.flickName1.value
-
-  const flicksDiv2 = document.querySelector('#flicks2')
-  flicksDiv2.textContent += ' '+f.flickName2.value
-
-  f.reset()
+renderProperty(name, value) {
+  const span = document.createElement('span')
+  span.classList.add(name)
+  span.textContent = value
+  return span
 }
 
-form.addEventListener('submit', addItems)
+renderItem(flick) { 
+    const item = document.createElement('li')
+    item.classList.add('flick')
+  
+    // get the list of properties
+    const properties = Object.keys(flick)
+   
+    // loop over the properties
+    properties.forEach((propertyName)=> {
+      // build a span, and append it to the list
+      const span = this.renderProperty(propertyName, flick[propertyName])
+      item.appendChild(span)
+    })
+  
+   return item
+  }
+
+addItems(event){
+  const f = event.target
+
+  const flick ={
+    name: f.flickName.value,
+    chris: f.chrisName.value,
+  }
+  this.arr.push(flick)
+  console.log(this.arr)
+  const item = this.renderItem(flick)
+
+  const list = document.querySelector('#flicks')
+  list.appendChild(item)
+
+  f.reset()
+  f.flickName.focus()
+}
+}
+
+const app = new App()
